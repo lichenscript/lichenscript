c4cb903277b96c7f737037dc33d6145ec53552d5
Vincent Chan <okcdz@diverse.space>
2021-08-18 16:45:18 +0800

    feat: js of ocaml

--- bin/waterlang.ml
+++ bin/waterlang.ml
@@ -1,15 +1,3 @@
-open Core
-
-let compile filename =
-  printf "Compiling %s\n" filename
-
-let command =
-  Command.basic
-    ~summary:"Compiling Waterlang"
-    ~readme:(fun () -> "More detailed information")
-    Command.Param.(
-     map (anon ("filename" %: string))
-       ~f:(fun filename -> (fun () ->compile filename)))
 
 let () = 
-  Command.run ~version:"1.0" ~build_info:"RWO" command
+  print_endline "Hello world"
