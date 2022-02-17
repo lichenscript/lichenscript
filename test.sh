6697fc25b10a11624025e07e12ef33bfab11dab9
Vincent Chan <okcdz@diverse.space>
2022-02-17 21:17:41 +0800

    bump version to 0.0.11

--- test.sh
+++ test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+uname -a
+
 export LSC_RUNTIME="./runtime"
 export LSC_STD="./std"
 
 # FLAGS="--base ./examples --platform wasm32"
 FLAGS="--base ./examples"
+
 
 dune build
 _build/default/test/lichenscript_test.exe ./examples -C ./_build/default/bin/main.exe $@ \
