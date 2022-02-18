eff47342e7f1d9bd6a637dc559eebdd37d6ea252
Vincent Chan <okcdz@diverse.space>
2022-02-18 19:25:00 +0800

    fix: type error of binary of f32

--- lib/c_backend/transform.ml
+++ lib/c_backend/transform.ml
@@ -1145,12 +1145,12 @@ and transform_expression ?(is_move=false) ?(is_borrow=false) env expr =
         spec
 
       | _ -> (
-        let node_type = Type_context.deref_node_type env.ctx ty_var in
-        if Check_helper.is_i64 env.ctx node_type then
+        (* let node_type = Type_context.deref_node_type env.ctx ty_var in *)
+        if Check_helper.is_i64 env.ctx left_type then
           C_op.Expr.I64Binary(op, left'.expr, right'.expr)
-        else if Check_helper.is_f64 env.ctx node_type then
+        else if Check_helper.is_f64 env.ctx left_type then
           C_op.Expr.F64Binary(op, left'.expr, right'.expr)
-        else if Check_helper.is_f32 env.ctx node_type then
+        else if Check_helper.is_f32 env.ctx left_type then
           C_op.Expr.F32Binary(op, left'.expr, right'.expr)
         else
           C_op.Expr.I32Binary(op, left'.expr, right'.expr)
