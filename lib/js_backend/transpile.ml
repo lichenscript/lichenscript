febc5b8f1ea80d09f4b68b28e454f3e8843c5a20
Vincent Chan <okcdz@diverse.space>
2022-02-19 20:45:36 +0800

    feat: codegen function method

--- lib/js_backend/transpile.ml
+++ lib/js_backend/transpile.ml
@@ -27,6 +27,8 @@ type t = {
 
   global_name_map: (int, string) Hashtbl.t;
 
+  mutable scope: Scope.scope option;
+
   mutable indents: int;
   mutable line: int;
   mutable col: int;
@@ -55,6 +57,7 @@ let create ~ctx () =
     buffer;
     sourcemap;
     global_name_map;
+    scope = None;
     indents = 0;
     line = 1;
     col = 0;
@@ -65,6 +68,13 @@ let with_indents env f =
   env.indents <- env.indents + 1;
   let result = f env in
   env.indents <- prev;
+  result
+
+let with_scope env scope f =
+  let prev_scope = env.scope in
+  env.scope <- Some scope;
+  let result = f env in
+  env.scope <- prev_scope;
   result
 
 let print_indents env =
@@ -94,19 +104,16 @@ and transpile_class env cls =
   Hashtbl.set env.global_name_map ~key:name_id ~data:given_name;
 
   let transpile_class_method ~given_name _method =
-    let { Typedtree.Declaration. cls_method_body; _ } = _method in
-    ps env "function ";
-    ps env given_name;
-    ps env "() {\n";
-    with_indents env (fun env ->
-      List.iter
-        ~f:(fun stmt ->
-          print_indents env;
-          transpile_statement env stmt;
-        )
-        cls_method_body.body
-    );
-    ps env "}\n"
+    let { Typedtree.Declaration. cls_method_body; cls_method_params; cls_method_scope; _ } = _method in
+    with_scope env (Option.value_exn cls_method_scope) (fun env ->
+      ps env "function ";
+      ps env given_name;
+      ps env "(";
+      tranpile_function_params env cls_method_params;
+      ps env ") {\n";
+      transpile_function_body env cls_method_body;
+      ps env "}\n"
+    )
   in
 
   let names = 
@@ -252,24 +259,11 @@ and transpile_expression env expr =
     ps env "]";
   )
 
-  | Map _ -> ()
+  | Map _ -> ps env "new Map()"
 
   | Call { callee; call_params; _} -> (
-    match callee with
-    | { spec = Identifier (_, id); _ } -> (
-      (match Type_context.find_external_symbol env.ctx id with
-      | Some ext_name -> (
-        (* external method *)
-        ps env ext_name;
-      )
 
-      | _ -> (
-        let given_name = Hashtbl.find_exn env.global_name_map id in
-        ps env given_name
-      ));
-
-      ps env "(";
-
+    let transpile_params () =
       let params_len = List.length call_params in
       List.iteri
         ~f:(fun index item ->
@@ -279,10 +273,91 @@ and transpile_expression env expr =
           )
         )
         call_params;
+    in
+
+    match callee with
+    | { spec = Identifier (_, id); _ } -> (
+      (match Type_context.find_external_symbol env.ctx id with
+      | Some ext_name -> (
+        (* external method *)
+        ps env ext_name;
+      )
+
+      | _ -> (
+        let given_name =
+          Hashtbl.find_exn env.global_name_map id
+        in
+        ps env given_name
+      ));
+
+      ps env "(";
+
+      transpile_params ();
 
       ps env ")"
 
     )
+
+    | { spec = Member(expr, id); _ } ->
+      begin
+        let expr_type = Type_context.deref_node_type env.ctx expr.ty_var in
+        let member = Check_helper.find_member_of_type env.ctx ~scope:(Option.value_exn env.scope) expr_type id.pident_name in
+        match member with
+        | Some ((Method ({ spec = ClassMethod { method_is_virtual = true; _ }; _ }, _, _)), _) -> (
+          transpile_expression env expr;
+          ps env ".";
+          ps env id.pident_name;
+          ps env "(";
+          transpile_params ();
+          ps env ")"
+        )
+        | Some ((Method ({ id = method_id; spec = ClassMethod { method_get_set = None; _ }; _ }, _, _)), _) -> (
+          (* only class method needs a this_expr, this is useless for a static function *)
+          (* let this_expr = transform_expression ~is_borrow:true env expr in *)
+
+          match Type_context.find_external_symbol env.ctx method_id with
+          | Some ext_name -> (
+            ps env ext_name;
+            ps env ".call(";
+            transpile_expression env expr;
+            if (List.length call_params) > 0 then (
+              ps env ", ";
+              transpile_params ()
+            );
+            ps env ")"
+          )
+          | _ ->
+            let callee_node = Type_context.get_node env.ctx method_id in
+            let ctor_opt = Check_helper.find_typedef_of env.ctx callee_node.value in
+            let ctor = Option.value_exn ~message:"Cannot find typedef of class" ctor_opt in
+            let ctor_ty_id = ctor.id in
+            let global_name = Hashtbl.find_exn env.global_name_map ctor_ty_id in
+            (* C_op.Expr.ExternalCall(global_name, Some this_expr.expr, params) *)
+            ps env global_name;
+            ps env ".call(";
+            transpile_expression env expr;
+            if (List.length call_params) > 0 then (
+              ps env ", ";
+              transpile_params ()
+            );
+            ps env ")"
+        )
+
+        (* it's a static function *)
+        | Some (TypeDef { id = fun_id; spec = Function _; _ }, _) -> (
+          let callee_node = Type_context.get_node env.ctx fun_id in
+          let ctor_opt = Check_helper.find_typedef_of env.ctx callee_node.value in
+          let ctor = Option.value_exn ctor_opt in
+          let ctor_ty_id = ctor.id in
+          let global_name = Hashtbl.find_exn env.global_name_map ctor_ty_id in
+          ps env global_name;
+          ps env "(";
+          transpile_params ();
+          ps env ")"
+        )
+
+        | _ -> failwith "unrechable"
+      end
 
     | _ -> ps env "/* unimplemented */{}"
   )
@@ -307,11 +382,18 @@ and transpile_expression env expr =
     ps env name.pident_name
   )
 
-  | Index _
+  | Index (expr, index) -> (
+    transpile_expression env expr;
+    ps env "[";
+    transpile_expression env index;
+    ps env "]"
+  )
+
   | Unary _ -> ()
   | Binary (op, left, right) -> (
     transpile_expression env left;
     let open BinaryOp in
+    ps env " ";
     ps env (match op with
     | Equal -> "==="
     | NotEqual -> "!=="
@@ -330,6 +412,7 @@ and transpile_expression env expr =
     | Xor -> "^"
     | BitAnd -> "&"
     );
+    ps env " ";
     transpile_expression env right
   )
 
@@ -357,7 +440,7 @@ and transpile_expression env expr =
   | Block _ -> ()
 
   | Init init -> (
-    let { Typedtree.Expression. init_name = (_, name_id); _ } = init in
+    let { Typedtree.Expression. init_name = (_, name_id); init_elements; _ } = init in
     let given_name = Hashtbl.find_exn env.global_name_map name_id in
     ps env "{\n";
     with_indents env (fun env ->
@@ -365,9 +448,28 @@ and transpile_expression env expr =
       ps env "__proto__: ";
       ps env given_name;
       ps env ",\n";
-    ());
+
+      List.iter
+        ~f:(fun element ->
+          print_indents env;
+          match element with
+          | InitSpread expr -> (
+            ps env "...";
+            transpile_expression env expr;
+            ps env ",\n"
+          )
+
+          | InitEntry { init_entry_key; init_entry_value; _ } -> (
+            ps env init_entry_key.pident_name;
+            ps env ": ";
+            transpile_expression env init_entry_value;
+            ps env ",\n"
+          )
+        )
+        init_elements
+    );
     print_indents env;
-    ps env "}\n"
+    ps env "}"
   )
 
   | Match _ -> ()
@@ -376,18 +478,61 @@ and transpile_expression env expr =
 
 and transpile_function env _fun =
   let open Function in
-  ps env "function ";
-  tranpile_id env _fun.header.name _fun.header.name_loc;
-  ps env "() {\n";
-  with_indents env (fun env ->
-    List.iter
-      ~f:(fun stmt ->
-        print_indents env;
-        transpile_statement env stmt
+  with_scope env _fun.scope (fun env ->
+    ps env "function ";
+    tranpile_id env _fun.header.name _fun.header.name_loc;
+    ps env "(";
+
+    let fun_name, name_id = _fun.header.name in
+    Hashtbl.set env.global_name_map ~key:name_id ~data:fun_name;
+
+    tranpile_function_params env _fun.header.params;
+
+    ps env ") {\n";
+    transpile_function_body env _fun.body;
+    ps env "}\n"
+  )
+
+and tranpile_function_params env (params: Function.params) = 
+  let open Function in
+  let params_len = List.length params.params_content in
+  List.iteri
+    ~f:(fun index param ->
+      let name = param.param_name in
+      tranpile_id env name param.param_loc;
+      if index <> (params_len - 1) then (
+        ps env ", "
       )
-      _fun.body.body;
+    )
+    params.params_content;
+
+and transpile_function_body env (body: Block.t) =
+  let body_len = List.length body.body in
+  with_indents env (fun env ->
+    List.iteri
+      ~f:(fun index stmt ->
+        let open Typedtree.Statement in
+        let is_last = index = (body_len - 1) in
+        if is_last then (
+          match stmt.spec with
+          | Expr expr -> (
+            print_indents env;
+            ps env "return ";
+            transpile_expression env expr;
+            ps env ";\n"
+          )
+
+          | _ -> (
+            print_indents env;
+            transpile_statement env stmt
+          )
+        ) else (
+          print_indents env;
+          transpile_statement env stmt
+        )
+      )
+      body.body;
   );
-  ps env "}\n"
 
 and tranpile_id env (name, _) (loc: Loc.t) =
   env.sourcemap#add_location env.col 0 loc.start.line loc.start.column;
