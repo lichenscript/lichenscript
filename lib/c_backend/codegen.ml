0769b35983d1bf1917a12ef70db7523971f56ca3
Vincent Chan <okcdz@diverse.space>
2022-02-25 12:53:21 +0800

    feat: remove codegen utils

--- lib/c_backend/codegen.ml
+++ lib/c_backend/codegen.ml
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *)
-open Lichenscript_codegen_utils
 open Lichenscript_typing
 open Lichenscript_ir.Ir
 open Lichenscript_ir
