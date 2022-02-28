6abfc9564e45f9288b6fd772e750fe00e7498905
Vincent Chan <okcdz@diverse.space>
2022-02-28 14:42:36 +0800

    feat: add README to web package

--- prepublish.sh
+++ prepublish.sh
@@ -12,3 +12,6 @@ cp ./README.md npm/lichenscript/README.md
 rm -rf npm/lichenscript-web/dist
 mkdir -p npm/lichenscript-web/dist
 cp _build/default/web/lichenscript_web.bc.js npm/lichenscript-web/dist
+
+rm -f npm/lichenscript-web/README.md
+cp ./README.md npm/lichenscript-web/README.md
