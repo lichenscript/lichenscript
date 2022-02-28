d461ebd5b558b286eb9c86813584b3e039a059cf
Vincent Chan <okcdz@diverse.space>
2022-02-28 14:24:08 +0800

    feat: throw out parse and type check errors

--- npm/lichenscript-web/test/api.test.js
+++ npm/lichenscript-web/test/api.test.js
@@ -1,9 +1,10 @@
 const assert = require('assert');
 const lichenscript = require('..');
 
-describe('Array', function () {
+describe('Compiler', function () {
   describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
+
+    it('compile api', function () {
       const result = lichenscript.compile(`
       function main() {
         print("Hello World");
@@ -12,5 +13,21 @@ describe('Array', function () {
       const fun = new Function(result)
       fun();
     });
+
+    it('parse error', function () {
+      try {
+        const result = lichenscript.compile(`
+        function main() {
+          print("Hello World;
+        }
+        `);
+        const fun = new Function(result)
+        fun();
+      } catch (err) {
+        assert.strictEqual(Array.isArray(err.errors), true)
+        console.log(JSON.stringify(err.errors));
+      }
+    });
+
   });
 });
