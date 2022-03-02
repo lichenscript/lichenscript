18130e48d3c267f62fc68864971d8741e74dc5d5
Vincent Chan <okcdz@diverse.space>
2022-03-03 00:39:06 +0800

    refactor: hoist exception to module

--- lib/typing/typecheck.mli
+++ lib/typing/typecheck.mli
@@ -1,9 +1,5 @@
+open Lichenscript_parsing
 
-(* val type_check:
-	?type_provider:Type_provider.provider ->
-	?open_domains:(string array list) ->
-	Typedtree.program -> (Typedtree.program * Type_error.t list) *)
+type import_checker = Ast.Declaration.import -> unit
 
-(* val type_check: Type_context.t -> ?debug:bool -> unit -> Type_error.t list *)
-
-val typecheck_module: Type_context.t -> verbose:bool -> Typedtree.program -> unit
+val typecheck_module: Type_context.t -> import_checker:import_checker -> verbose:bool -> Typedtree.program -> unit
