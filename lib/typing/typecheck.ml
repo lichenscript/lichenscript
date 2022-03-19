2e28c4a30257ab1ae1d2b805bc9927c65f00468a
Vincent Chan <okcdz@diverse.space>
2022-03-20 04:28:31 +0800

    fix: tests of f64

--- lib/typing/typecheck.ml
+++ lib/typing/typecheck.ml
@@ -701,7 +701,8 @@ and check_expression env expr =
     | Asttypes.UnaryOp.Minus ->
       if not (Check_helper.is_i32 env.ctx node_type) &&
         not (Check_helper.is_i64 env.ctx node_type) &&
-        not (Check_helper.is_f32 env.ctx node_type) then (
+        not (Check_helper.is_f32 env.ctx node_type) &&
+        not (Check_helper.is_f64 env.ctx node_type) then (
         raise_err ()
       );
       Program.update_node_type env.ctx expr.ty_var node_type
